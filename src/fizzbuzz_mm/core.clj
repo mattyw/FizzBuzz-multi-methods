7e849377ce60b31bf442eea150ed32a891080f81
Matthew Williams <mwilliams@HVTL0158.(none)>
2012-01-27 10:15:45 +0800

    implemented with multimethods

--- src/fizzbuzz_mm/core.clj
+++ src/fizzbuzz_mm/core.clj
@@ -1,8 +1,28 @@
 (ns fizzbuzz_mm.core)
 
-(defn fizzbuzz [x]
+
+
+(defn dispatch [x]
   (cond
-   (= 0 (mod x 15)) "fizzbuzz"
-   (= 0 (mod x 3)) "fizz"
-   (= 0 (mod x 5)) "buzz"
-   (number? x) x))
+   (= 0 (mod x 15)) :fizzbuzz
+   (= 0 (mod x 3)) :fizz
+   (= 0 (mod x 5)) :buzz
+   (number? x) :number))
+
+(defmulti fizzbuzz dispatch)
+
+(defmethod fizzbuzz :number [x]
+  x)
+
+(defmethod fizzbuzz :fizzbuzz [x]
+  "fizzbuzz")
+
+(defmethod fizzbuzz :fizz [x]
+  "fizz")
+
+(defmethod fizzbuzz :buzz [x]
+  "buzz")
+
+
+	   
+  
